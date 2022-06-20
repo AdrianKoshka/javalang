3fba697137b0c97de5e4492f7d8f6b5ad2448b82
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2022-06-19 20:41:48 -0400

    Add the ability to download the JRE instead of the JDK

--- gen-build-dir.py
+++ gen-build-dir.py
@@ -38,8 +38,8 @@ def copy_templates(operatingSystem: str, architecture: str, javaVersion: str) ->
     with open(pyproject_destination, 'w') as configfile:
         config.write(configfile)
 
-def get_JDK_release(operatingSystem: str, architecture: str, javaVersion: str) -> None:
-    match javaVersion:
+def get_JDK_release(operatingSystem: str, architecture: str, java_version: str, jre_or_jdk: bool) -> None:
+    match java_version:
         case '17.0.3+7':
             github_url = 'https://github.com/adoptium/temurin17-binaries'
             open_jdk_slug = 'OpenJDK17U'
@@ -52,12 +52,17 @@ def get_JDK_release(operatingSystem: str, architecture: str, javaVersion: str) -
             print(
                 "Unsupported version of java, supported versions are 17.0.3+7 and 18.0.1+10")
             exit(1)
+    if jre_or_jdk:
+        open_jdk_edition = 'jre'
+        jdk_extract_dir = os.path.join(f'{operatingSystem}-{architecture}-javalang', 'src', f'jdk-{java_version}-jre')
+    else:
+        open_jdk_edition = 'jdk'
+        jdk_extract_dir = os.path.join(f'{operatingSystem}-{architecture}-javalang', 'src', f'jdk-{java_version}')
     src_dir = os.path.join(f'{operatingSystem}-{architecture}-javalang', 'src')
     javalang_dir = os.path.join(f'{operatingSystem}-{architecture}-javalang', 'src', 'javalang')
-    jdk_extract_dir = os.path.join(f'{operatingSystem}-{architecture}-javalang', 'src', f'jdk-{javaVersion}')
     match operatingSystem:
         case "windows":
-            java_url = f'{github_url}/releases/download/jdk-{javaVersion}/{open_jdk_slug}-jdk_{architecture}_{operatingSystem}_hotspot_{open_jdk_ver_slug}.zip'
+            java_url = f'{github_url}/releases/download/jdk-{java_version}/{open_jdk_slug}-{open_jdk_edition}_{architecture}_{operatingSystem}_hotspot_{open_jdk_ver_slug}.zip'
             win_zip_location = os.path.join(f'{operatingSystem}-{architecture}-javalang', 'src', 'jdk.zip')
             if os.path.exists(win_zip_location):
                 print("JDK already downloaded, skipping download step")
@@ -68,12 +73,15 @@ def get_JDK_release(operatingSystem: str, architecture: str, javaVersion: str) -
                 os.remove(win_zip_location)
                 os.rename(jdk_extract_dir, javalang_dir)
         case _:
-            java_url = f'{github_url}/releases/download/jdk-{javaVersion}/{open_jdk_slug}-jdk_{architecture}_{operatingSystem}_hotspot_{open_jdk_ver_slug}.tar.gz'
+            java_url = f'{github_url}/releases/download/jdk-{java_version}/{open_jdk_slug}-{open_jdk_edition}_{architecture}_{operatingSystem}_hotspot_{open_jdk_ver_slug}.tar.gz'
             tar_gz_location = os.path.join(f'{operatingSystem}-{architecture}-javalang', 'src', 'jdk.tar.gz')
             if os.path.exists(tar_gz_location):
                 print("JDK already downloaded, skipping download step")
             else:
-                print("Downloading JDK")
+                if jre_or_jdk:
+                    print("Downloading JRE")
+                else:
+                    print("Downloading JDK")
                 urllib.request.urlretrieve(java_url, tar_gz_location)
                 print("Download Finished")
                 with tarfile.open(tar_gz_location, 'r') as jdk_tar:
@@ -90,11 +98,12 @@ def main() -> None:
                         type=str, help='Specify an architecture')
     parser.add_argument("-j", "--java-version", metavar='java_ver',
                         type=str, help='Specify a version of java (17.0.3+7 or 18.0.1+10)', default='17.0.3+7')
+    parser.add_argument("-r", "--jre", action='store_true')
     
     args = parser.parse_args()
 
     make_project_dirs(args.operating_system, args.architecture)
-    get_JDK_release(args.operating_system, args.architecture, args.java_version)
+    get_JDK_release(args.operating_system, args.architecture, args.java_version, args.jre)
     copy_templates(args.operating_system, args.architecture, args.java_version)
 
 main()
